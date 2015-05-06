84d11262dc443dbc1ff33fd351b4f914ce179885
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2015-05-06 15:42:48 +0200

    fixes and preparation for new search

--- editorsnotes/search/management/commands/rebuild_es_index.py
+++ editorsnotes/search/management/commands/rebuild_es_index.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from ... import search
+from editorsnotes.search import en_index
 
 class Command(BaseCommand):
     help = 'Rebuild elasticsearch cendari index'
