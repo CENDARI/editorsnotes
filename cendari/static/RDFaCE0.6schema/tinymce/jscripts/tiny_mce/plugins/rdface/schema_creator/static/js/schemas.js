576219575109389ca2ab61d86dbb3ea31b196beb
nboukhelifa <nadia.boukhelifa@gmail.com>
2015-06-22 17:49:21 +0200

    trying autocomplete with geonames webservice

--- cendari/static/RDFaCE0.6schema/tinymce/jscripts/tiny_mce/plugins/rdface/schema_creator/static/js/schemas.js
+++ cendari/static/RDFaCE0.6schema/tinymce/jscripts/tiny_mce/plugins/rdface/schema_creator/static/js/schemas.js
@@ -110,15 +110,47 @@ function create_form(schema,property,container){
 			"cd",
 			"d"
             	]; 
-		$( "#entity_freebase" ).autocomplete({
-  			source: dummyList,	
-			minLength: 1,		 
-			select: function( event, ui ) {
-        			console.log( ui.item ?
-          			"Selected: " + ui.item.value + " aka " + ui.item.id :
-          			"Nothing selected, input was " + this.value );
-      			}
+		//$( "#entity_freebase" ).autocomplete({
+  		//	source: dummyList,	
+		//	minLength: 1,		 
+		//	select: function( event, ui ) {
+        	//		console.log( ui.item ?
+          	//		"Selected: " + ui.item.value + " aka " + ui.item.id :
+          	//		"Nothing selected, input was " + this.value );
+      		//	}
+		//});
+
+
+	   $( "#entity_freebase" ).autocomplete({
+	      source: function( request, response ) {
+		$.ajax({
+		  url: "http://gd.geobytes.com/AutoCompleteCity",
+		  dataType: "jsonp",
+		  data: {
+		    q: request.term
+		  },
+		  success: function( data ) {
+		    response( data );
+		  }
 		});
+	      },
+	      minLength: 3,
+	      select: function( event, ui ) {
+		console.log( ui.item ?
+		  "Selected: " + ui.item.label :
+		  "Nothing selected, input was " + this.value);
+	      },
+	      open: function() {
+		$( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+	      },
+	      close: function() {
+		$( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+	      }
+	    });
+
+
+
+
 		$('#entity_freebase').mouseover(function() {
 			   $('#entity_freebase').tooltip({
 			        placement : 'right',
