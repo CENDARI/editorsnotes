8b456d2e8c2ad00974295b39cd852c8490ea5403
mangia <em.giannisakis@gmail.com>
2015-07-20 19:58:23 +0200

    fixes

--- cendari/semantic.py
+++ cendari/semantic.py
@@ -447,7 +447,7 @@ def semantic_process_note(note,user=None):
 				explicit_date = results_reg[1:len(results_reg)-1]
 				if utils.parse_well_known_date(explicit_date):
                 			topic.date = utils.parse_well_known_date(explicit_date)
-					topic.rdf = explicit_date
+					# topic.rdf = explicit_date
                 			logger.debug('Found a valid date between []: %s', topic.date)
 					topic.save()	
     semantic.commit()
@@ -506,7 +506,7 @@ def semantic_process_document(document,user=None):
             elif t['type']=='EVT':
          	if utils.parse_well_known_date(value):
                 	topic.date = utils.parse_well_known_date(value)
-			topic.rdf = value
+			# topic.rdf = value
                 	logger.debug('Found a valid date: %s', topic.date)
 			topic.save()	
  		else:
@@ -516,7 +516,7 @@ def semantic_process_document(document,user=None):
 				explicit_date = results_reg[1:len(results_reg)-1]
 				if utils.parse_well_known_date(explicit_date):
                 			topic.date = utils.parse_well_known_date(explicit_date)
-					topic.rdf = explicit_date
+					# topic.rdf = explicit_date
                 			logger.debug('Found a valid date between []: %s', topic.date)
 					topic.save()	
     semantic.commit()
