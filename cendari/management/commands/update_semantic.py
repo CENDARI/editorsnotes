67a4f430af72491eeb5b1b064bf2337df228049a
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2015-10-05 18:56:56 -0400

    improve printed messages

--- cendari/management/commands/update_semantic.py
+++ cendari/management/commands/update_semantic.py
@@ -13,16 +13,20 @@ class Command(NoArgsCommand):
                 self.stdout.write(self.style.NOTICE('%d' % note.id), ending=' ')
             semantic_process_note(note)
         if options['verbosity'] > 0:
-            self.stdout.write('')
+            self.stdout.write('\n')
         if options['verbosity'] > 0:
-            self.stdout.write(self.style.NOTICE('Updating topics:'), ending=' ')
+            self.stdout.write(self.style.NOTICE('Updating documents:'), ending=' ')
         for document in Document.objects.all():
             if options['verbosity'] > 1:
                 self.stdout.write(self.style.NOTICE('%d' % document.id), ending=' ')
             semantic_process_document(document)
+        if options['verbosity'] > 0:
+            self.stdout.write('\n')
+        if options['verbosity'] > 0:
+            self.stdout.write(self.style.NOTICE('Updating topics:'), ending=' ')
         for topic in Topic.objects.all():
             if options['verbosity'] > 1:
                 self.stdout.write(self.style.NOTICE('%d' % topic.id), ending=' ')
             semantic_process_topic(topic)
         if options['verbosity'] > 0:
-            self.stdout.write('')
+            self.stdout.write('\n')
