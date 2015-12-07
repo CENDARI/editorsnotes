775aa7093389ecda25280ce79739538e222ae868
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2015-12-07 07:43:07 -0500

    flush nte documents selectively

--- cendari/search/management/commands/flush_cendari_documents.py
+++ cendari/search/management/commands/flush_cendari_documents.py
@@ -2,7 +2,30 @@ from django.core.management.base import BaseCommand
 
 from ... import cendari_index
 
+from elasticsearch import Elasticsearch, helpers
+
 class Command(BaseCommand):
     help = 'Remove elasticsearch cendari document index'
+
     def handle(self, *args, **kwargs):
-        cendari_index.delete_document()
+        #cendari_index.delete_document()
+        es = Elasticsearch()
+        query_body = {
+            "query": {
+                "term": { "application" : "nte" }
+            }
+        }
+        scaniter = helpers.scan(client=es, query=query_body, scroll="1m",
+                                index='cendari', doc_type='document')
+
+        def del_iterator():
+            for rec in scaniter:
+                yield { '_op_type': 'delete',
+                        '_index': 'cendari',
+                        '_type': 'document',
+                        '_id': rec['_id']
+                }
+
+        #with open('delete.out', 'wb') as out:
+        ret = helpers.bulk(es, del_iterator())
+        #out.write(json.dumps(ret))
--- cendari/search/management/commands/flush_cendari_entity.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from ... import cendari_index
-
-class Command(BaseCommand):
-    help = 'Remove elasticsearch cendari document index'
-    def handle(self, *args, **kwargs):
-        cendari_index.delete_entity()
